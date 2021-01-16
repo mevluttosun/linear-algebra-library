5117b705c31e7d61ccab7b655118a6786d75f889
mevluttosun <mevlutt2017@gmail.com>
2021-01-16 20:23:16 +0300

    matrix size

--- test.cpp
+++ test.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 
 int main() {
-	double *d=(double *)malloc(5*sizeof(double));
+	double *d=(double *)malloc(6*sizeof(double));
 	d[0]=3.1;d[1]=5.6;d[2]=9.2;d[3]=2.4;d[4]=5.2;d[5]=6.3;
 	Vector *v = new Vector(d,5);
-	v->print();
+	//v->print();
 	Matrix *m= new Matrix(d,3,3);
+	m->print();
           return 0;
 }
